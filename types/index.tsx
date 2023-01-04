4bf0aabd8b12eb40dc1b693fcde3f95375f6e9ae
Jahanzaib Tayyab <jahanzaib.tayyab@devigital.com>
2023-01-04 22:32:18 +0500

    add dummy data and static images

---
+++ types/index.tsx
@@ -0,0 +1,16 @@
+export type exploreWorld = {
+  id: string;
+  imgUrl: string;
+  title: string;
+};
+
+export type newFeature = {
+  imgUrl: string;
+  title: string;
+  subtitle: string;
+};
+
+export type social = {
+  name: string;
+  url: string;
+};
