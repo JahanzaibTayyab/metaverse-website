72e3a5c5473e743c17da147ef84a5bfdefbdedec
Jahanzaib Tayyab <jahanzaib.tayyab@devigital.com>
2023-01-05 17:40:01 +0500

    fix explore card issue

--- components/ExploreCard.tsx
+++ components/ExploreCard.tsx
@@ -34,8 +34,8 @@ const ExploreCard: NextPage<Props> = ({
       src={imgUrl}
       alt={title}
       className="absolute w-full h-full object-cover rounded-[24px]"
-      width={3600}
-      height={1600}
+      width={3000}
+      height={1200}
     />
     {active !== id ? (
       <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]">
